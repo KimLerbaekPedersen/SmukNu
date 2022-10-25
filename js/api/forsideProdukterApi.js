6e8e10295c4b40c85652f69241d3035706f6b607
Kim Lerbæk Pedersen <kilepe@hotmail.com>
2022-10-25 22:56:13 +0200

    forsideProdukter done

--- js/api/forsideProdukterApi.js
+++ js/api/forsideProdukterApi.js
@@ -4,27 +4,29 @@ Promise.all([
             Accept: 'application/json'
         }
     })
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function(recommendedData) {
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (recommendedData) {
 
             let product = "";
             for (let i = 0; i < recommendedData.length; i++) {
-                    if(recommendedData[i].recommended == true || recommendedData[i].discountInPercent == 'string') {                      
-                product += `
+                if (recommendedData[i].recommended == true) {
+                    product += `
                     <div class='product-api-container'>
+                    <div class='discount-container'>
+                        ${recommendedData[i].discountInPercent == '' ? '' : `<p class='discount'>spar ${recommendedData[i].discountInPercent}%</p>`} 
+                    </div>    
                         <img class='product-image'src='${recommendedData[i].image}'>
                             <div class='product-headline'>
-                                <p class='discount'>spar ${recommendedData[i].discountInPercent}%</p>
                                 <p class='title'>${recommendedData[i].title}</p>
                                 <p class='product-price'>${recommendedData[i].price},00kr.</p>
                             </div>
                     </div>
                 `
+                }
             }
-        }
             document.querySelector('#product-container').innerHTML = product;
         }
-    ),
+        ),
 ]);
